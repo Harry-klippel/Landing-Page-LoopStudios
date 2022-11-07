9b4fad1de10ed5c32afaea4062e1fb76e9d3a2ba
Harry Klippel <harryklippel21@gmail.com>
2022-11-07 02:37:53 -0300

    adicionando modo responsivo

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-let houver = document.querySelectorAll('a')
+let nav1 = document.querySelectorAll('#t1')
+let nav2 = document.querySelectorAll('#t2')
+let social = document.querySelectorAll('#t3')
 
-houver.forEach(function(elemento){
+
+nav1.forEach(function(content){
    
+    content.addEventListener("mouseover", function() {
+       content.classList.add("teste")
+    })
+    
+    content.addEventListener("mouseout", function() {
+        content.classList.remove("teste")
+    })
+    console.log(content)
+})
 
+nav2.forEach(function(elemento){
+   
     elemento.addEventListener("mouseover", function() {
-       elemento.classList.add("teste")
+       elemento.classList.add("nav2")
     })
     
     elemento.addEventListener("mouseout", function() {
-        elemento.classList.remove("teste")
+        elemento.classList.remove("nav2")
     })
 })
+
+
+/*social.forEach(function(very){
+   
+    very.addEventListener("click", function() {
+        alert("oi")
+       very.classList.add("social2")
+    })
+    
+    very.addEventListener("mouseout", function() {
+        very.classList.remove("social2")
+    })
+})*/
 
