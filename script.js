cd206a4db9c1d781723ad6b3d584b168b7499838
Harry Klippel <harryklippel21@gmail.com>
2022-11-24 22:05:35 -0300

    final

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let nav1 = document.querySelectorAll('#t1')
 let nav2 = document.querySelectorAll('#t2')
 let social = document.querySelectorAll('#t3')
-
+let menu = document.querySelector('#mobileMenu')
+let burger = document.querySelector('#material-symbols-outlined')
+let closeMenu = document.querySelector('#closeMenu')
 
 nav1.forEach(function(content){
    
@@ -27,15 +29,23 @@ nav2.forEach(function(elemento){
 })
 
 
-/*social.forEach(function(very){
+social.forEach(function(elemento){
    
-    very.addEventListener("click", function() {
-        alert("oi")
-       very.classList.add("social2")
+    elemento.addEventListener("mouseover", function() {
+       elemento.classList.add("social2")
     })
     
-    very.addEventListener("mouseout", function() {
-        very.classList.remove("social2")
+    elemento.addEventListener("mouseout", function() {
+        elemento.classList.remove("social2")
     })
-})*/
+})
 
+burger.onclick = function() {
+    menu.style.display = 'block'
+
+}
+
+closeMenu.onclick = function() {
+    menu.style.display = 'none'
+
+}
